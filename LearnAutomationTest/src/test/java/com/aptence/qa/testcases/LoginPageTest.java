5872fb0c4424b285e0ba1085b2921ff77a2992b9
Jashandeep <Jashandeep@192.168.0.106>
2019-10-29 19:50:31 +0530

    Removed unnecessary comments

--- LearnAutomationTest/src/test/java/com/aptence/qa/testcases/LoginPageTest.java
+++ LearnAutomationTest/src/test/java/com/aptence/qa/testcases/LoginPageTest.java
@@ -28,25 +28,25 @@ public class LoginPageTest extends TestBase{
 		return data;
 	}
 	
-//	@Test(priority=1, dataProvider = "getLoginData")
-//	public void loginTest(String eusername, String epassword) {	//dataprovider fetches data from getLoginData() so excel username and password we have to enter here
-//		loginpage.multipleLogin(eusername, epassword);
-//	}
-//	
-//	@Test(priority=2)
-//	public void loginPageTitleTest() {
-//		String title =loginpage.validateLoginPageTitle();
-//		Assert.assertEquals(title, "Aptence | Adaptive, Gamified & Rewarding Learning Platform for School Students");
-//		}
-//	@Test(priority=3)
-//	public void loginPageLogoTest() {
-//		Boolean flag = loginpage.validateLoginPageLogo();
-//		Assert.assertTrue(flag);
-//	}
-//	@Test(priority=4)
-//	public void loginPagePopUpTest() {
-//		loginpage.closePopUp();
-//	}
+	@Test(priority=1, dataProvider = "getLoginData")
+	public void loginTest(String eusername, String epassword) {	//dataprovider fetches data from getLoginData() so excel username and password we have to enter here
+		loginpage.multipleLogin(eusername, epassword);
+	}
+	
+	@Test(priority=2)
+	public void loginPageTitleTest() {
+		String title =loginpage.validateLoginPageTitle();
+		Assert.assertEquals(title, "Aptence | Adaptive, Gamified & Rewarding Learning Platform for School Students");
+		}
+	@Test(priority=3)
+	public void loginPageLogoTest() {
+		Boolean flag = loginpage.validateLoginPageLogo();
+		Assert.assertTrue(flag);
+	}
+	@Test(priority=4)
+	public void loginPagePopUpTest() {
+		loginpage.closePopUp();
+	}
 	@Test(priority=5)
 	public void loginPageLoginFormTest() {
 		homepage=loginpage.loginup(prop.getProperty("username"), prop.getProperty("password"));
